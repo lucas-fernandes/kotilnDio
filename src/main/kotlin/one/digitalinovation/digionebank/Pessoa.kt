1a688edc65cafe668a23406eefc35b67ed550226
lucas fernandes <luks.faf@gmail.com>
2021-07-26 21:34:14 -0300

    update to apply the private modifier to set function of cpf property

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -2,13 +2,24 @@ package one.digitalinovation.digionebank
 
 class Pessoa {
     var nome:String = "Lucas Fernandes"
+
     var cpf:String = "999.999.999-99"
+    private set
+
+    /** Classe interna
+        inner class Endereco{
+            var rua:String = "Rua teste"
+        }
+    **/
 
 }
 
 fun main(){
     val lucas = Pessoa()
 
+
+
     println(lucas.nome)
     println(lucas.cpf)
+
 }
