49e9b99c294f15e63819ea1eed59e0220051a09f
lucas fernandes <luks.faf@gmail.com>
2021-07-27 22:39:29 -0300

    update to make person and employee abstract class

---
+++ src/main/kotlin/one/digitalinovation/digionebank/Funcionario.kt
@@ -0,0 +1,11 @@
+package one.digitalinovation.digionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+     nome: String,
+     cpf: String,
+    val salario: BigDecimal,
+) : Pessoa(nome, cpf) {
+    abstract fun calculoBonus():
+}
--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -1,26 +1,6 @@
 package one.digitalinovation.digionebank
 
-class Pessoa {
-    var nome:String = "Lucas Fernandes"
-
-    var cpf:String = "999.999.999-99"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "Nome: $nome, CPF: $cpf"
-
-    /** Classe interna
-        inner class Endereco{
-            var rua:String = "Rua teste"
-        }
-    **/
-
-}
-
-fun main(){
-    val lucas = Pessoa()
-
-    println(lucas.pessoaInfo())
-
-}
+abstract class Pessoa(
+     val nome: String,
+     val cpf: String
+)
